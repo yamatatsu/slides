dd66bde4fa0875b5829e813876402fc7f373e0b2
yamatatsu <the.kinnikuman@gmail.com>
2018-12-03 20:09:07 +0900

    monokai

--- slides/2018-11-28_immutable/components/SandBox/Editor.js
+++ slides/2018-11-28_immutable/components/SandBox/Editor.js
@@ -2,16 +2,16 @@ import React from 'react'
 import brace from 'brace';
 import AceEditor from 'react-ace';
 
-import 'brace/mode/java';
-import 'brace/theme/github';
+import 'brace/mode/typescript';
+import 'brace/theme/monokai';
 
 export default function Editor(props) {
   const { code, onChange, commands } = props
   return (
     <AceEditor
       width='600px'
-      mode="javascript"
-      theme="github"
+      mode="typescript"
+      theme="monokai"
       onChange={onChange}
       value={code}
       name="UNIQUE_ID_OF_DIV"
