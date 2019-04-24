cb1fa9f97196944f38f88682bae70f4030255968
yamatatsu <the.kinnikuman@gmail.com>
2019-04-25 02:06:02 +0900

    prerenderring

--- slides/2019-04-17_typescript/webpack.config.js
+++ slides/2019-04-17_typescript/webpack.config.js
@@ -1,5 +1,9 @@
+const path = require('path')
 const webpack = require('webpack')
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
+const PrerenderSPAPlugin = require('prerender-spa-plugin')
+
+const { npm_package_slideKey } = process.env
 
 module.exports = {
   resolve: {
@@ -9,6 +13,16 @@ module.exports = {
     new webpack.IgnorePlugin(/^fs$/),
     new MonacoWebpackPlugin({
       languages: ['typescript'],
+    }),
+    new PrerenderSPAPlugin({
+      // `dist/${npm_package_slideKey}` に置いてある index.html を SPA として読み込み
+      staticDir: path.join(__dirname, '../../dist', npm_package_slideKey),
+      // SPA 内で `/${npm_package_slideKey}` にアクセスして
+      routes: [`/${npm_package_slideKey}`],
+      // 'dist' に snapshot を置く。
+      // この時、routesで指定した `/${npm_package_slideKey}` に置くため、
+      // `dist/${npm_package_slideKey}/index.html` が上書きされる
+      outputDir: path.join(__dirname, '../../dist'),
     }),
   ],
   module: {
