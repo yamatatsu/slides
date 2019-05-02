183ca3de9974c976429426e13e541c6bad7bf760
yamatatsu <the.kinnikuman@gmail.com>
2019-05-02 16:38:41 +0900

    remove all slide sources

--- slides/2018-09-14_pipeline/assets/codes/ramda_map.js
+++
@@ -1,4 +0,0 @@
-const data = [1, 2, 3]
-
-const { map } = R
-alert(map(n => n * 2, data))
--- slides/2018-09-14_pipeline/webpack.config.js
+++
@@ -1,7 +0,0 @@
-const webpack = require('webpack')
-
-module.exports = {
-  plugins: [
-    new webpack.IgnorePlugin(/^fs$/),
-  ],
-};
